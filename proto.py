bc2ef31b68bf4f08fde7e2f986c22ee7e8061f38
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-02-18 02:11:57 +0100

    added more message

--- proto.py
+++ proto.py
@@ -7,8 +7,10 @@ def sendMessage( message ):
     rbl_message = raspberrylife_pb2.RBLMessage()
     rbl_message.id = "observer_module"
     rbl_message.messageType = addressbook_pb2.MessageType.RUN_INSTRUCTION;
+    run_instruction = rbl_message.runInstruction.add()
+    runInstruction.
 
-    print(message);
+    print( rbl_message );
     return;
 
-sendMessage("hallo");
+sendMessage( "hallo" );
