1735e45146ac42508bc7f81ef9b24d9063b096ff
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-02-18 16:20:19 +0100

    added complete message definition

--- proto.py
+++ proto.py
@@ -6,11 +6,18 @@ import raspberrylife_pb2 as proto
 def sendMessage( message ):
     rbl_message = raspberrylife_pb2.RBLMessage()
     rbl_message.id = "observer_module"
-    rbl_message.messageType = addressbook_pb2.MessageType.RUN_INSTRUCTION;
+    rbl_message.messageType = raspberrylife_pb2.MessageType.RUN_INSTRUCTION;
     run_instruction = rbl_message.runInstruction.add()
-    runInstruction.
+    actuator = runInstruction.Actuator();
+    actuator.actuatorType = raspberrylife_pb2.ActuatorType.CLIENT;
+    actuator.actuatorId = 0; # id=0 for admin user
+    instruction = runInstruction.Instruction();
+    instruction.instructionId = 0; # id=0 for user notification
+    param = instruction.parameters.add()
+    param = "Intruder detected" +  message;
 
     print( rbl_message );
+
     return;
 
 sendMessage( "hallo" );
